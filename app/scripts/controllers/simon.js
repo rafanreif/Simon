2584bb54dd72600ddd55f4977430bf329dcce10e
rafanreif <rafandresros@gmail.com>
2016-01-28 19:24:52 +0100

    sumar colores

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -14,13 +14,59 @@ angular.module('simonApp')
 
   		$scope.titulo = 'Simón';
 
+      $scope.acumuladorrojo = 0;
+      $scope.acumuladorverde = 0;
+      $scope.acumuladoramarillo = 0;
+      $scope.acumuladorazul = 0;
+
 
   		$scope.play = function(color){
-  			$scope.colores.push(color);
-  		}
+  		  $scope.colores.push(color);
+        
+        if(color == 'verde'){
+          $scope.acumuladorverde = $scope.acumuladorverde + 1;
+        }
 
-  		$scope.borrar = function(){
-  			$scope.colores = [];
-  		}
+         if(color == 'rojo'){
+          $scope.acumuladorrojo = $scope.acumuladorrojo + 1;
+        }
+      
+  		  if(color == 'azul'){
+          $scope.acumuladorazul = $scope.acumuladorazul + 1;
+        }
+
+        if(color == 'amarillo'){
+          $scope.acumuladoramarillo = $scope.acumuladoramarillo + 1;
+        }
+
+        /*Otra forma de hacerlo
+
+        switch(color){
+          case 'verde': 
+            $scope.acumuladorverde = $scope.acumuladorverde + 1;
+            break;
+          case 'rojo': 
+            $scope.acumuladorrojo = $scope.acumuladorrojo + 1;
+            break;
+          case 'azul': 
+            $scope.acumuladorazul = $scope.acumuladorazul + 1;
+            break;
+          case 'amarillo': 
+            $scope.acumuladoramarillo = $scope.acumuladoramarillo + 1;
+            break;
+        } */
+
+      }
+
+  		
+
+      $scope.borrar = function(){
+        $scope.colores = [];
+        $scope.acumuladorrojo = 0;
+        $scope.acumuladorverde = 0;
+        $scope.acumuladorazul = 0;
+        $scope.acumuladoramarillo = 0;
+  		
+      }
 
   });
